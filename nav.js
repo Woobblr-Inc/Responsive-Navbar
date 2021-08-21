1f1bb13aafec9c329834e5a5b66a0416f3296cdb
Rohan Puranik <59736919+Rohan-3@users.noreply.github.com>
2021-08-21 22:10:54 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,10 @@
+const navslide =() =>{
+    const burger = document.querySelector(".burger")
+    const nav = document.querySelector('nav ul')
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('navclick'); 
+
+    })
+}
+
+navslide();
